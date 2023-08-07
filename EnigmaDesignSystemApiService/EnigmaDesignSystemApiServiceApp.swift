82cbca59db9f90cf7968834d0d2b1494c098caae
Javad <sir.hxt@gmail.com>
2023-08-07 21:07:52 +0330

    Initial Commit

---
+++ EnigmaDesignSystemApiService/EnigmaDesignSystemApiServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnigmaDesignSystemApiServiceApp.swift
+//  EnigmaDesignSystemApiService
+//
+//  Created by Javad on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EnigmaDesignSystemApiServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
